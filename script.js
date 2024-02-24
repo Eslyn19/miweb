c0c0088c9b6771fa9f0e9130d9f2a123c2e6033b
Eslyn19 <andresjara479@gmail.com>
2024-02-24 15:53:19 -0600

    Added new change

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ console.log("Hello World");
 
 const arr = [0, 1];
 
-for (let i = 2; i < 7; i++){
-  arr.push(arr[i - 1] + arr[i - 2]);
+function Fibonacci (arr) {
+  for (let i = 2; i < 7; i++){
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr;  
 }
 
-console.log(arr);
+console.log(Fibonacci(arr));
